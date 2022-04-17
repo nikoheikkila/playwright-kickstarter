21708197f47b6ddd8180786ac914e48e16ec1222
Niko Heikkilä <niko.heikkila@futurice.com>
2022-04-17 13:44:54 +0300

    feat(e2e): update Playwright dependencies and configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,39 +1,89 @@
+import os from 'node:os';
 import { PlaywrightTestConfig, devices } from '@playwright/test'
 
+// Default development URL - change if necessary
+const DEV_URL = 'http://localhost:3000';
+
+// Check whether tests are running as part of a CI/CD pipeline
+const isPipeline = !!process.env.CI;
+
+// Match pixel comparison at least 95 % to avoid flaky tests but ensure enough confidence
+const threshold = 0.95;
+
+// GitHub Actions only has 2 vCPUs available, hence we want to use them all.
+const workers = isPipeline ? 2 : undefined;
+
+// Ensure we are not wasting build minutes in CI by operating in a fail-fast mode
+const maxFailures = isPipeline ? 1 : undefined;
+
+// Increase the default test timeout to 60 seconds to allow more resiliency
+const timeout = 60 * 1000;
+
+// Potentially interesting metadata to append into the test report – might help with debugging
+const metadata: Record<string, string> = {
+    cpu: os.arch(),
+    memory: `${os.totalmem() / (1024 ** 2)} MB`,
+    hostname: os.hostname(),
+    system: os.type(),
+    kernel: os.version(),
+};
+
 const config: PlaywrightTestConfig = {
+    name: 'Playwright Acceptance Test Suite',
+    preserveOutput: 'failures-only',
     testDir: 'tests',
-    outputDir: 'results',
-    forbidOnly: !!process.env.CI,
-    reporter: 'list',
+    fullyParallel: true,
+    forbidOnly: isPipeline,
+    reporter: isPipeline ? [['github']] : [['list'], ['html']],
     retries: 1,
+    timeout,
+    workers,
+    metadata,
+    maxFailures,
     webServer: process.env.APP_URL
         ? undefined
         : {
             command: 'yarn serve',
-            port: 3000,
-            reuseExistingServer: true,
+            url: DEV_URL,
+            reuseExistingServer: !isPipeline,
+            timeout: 120 * 1000,
         },
     use: {
-        baseURL: process.env.APP_URL ?? 'http://localhost:3000',
-        screenshot: process.env.CI ? 'only-on-failure' : 'off',
-        video: process.env.CI ? 'retain-on-failure' : 'off',
-        trace: process.env.CI ? 'retain-on-failure' : 'off',
+        baseURL: process.env.APP_URL ?? DEV_URL,
+        userAgent: 'Microsoft Playwright',
+        locale: 'en-GB',
+        colorScheme: 'dark',
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure',
+        trace: 'retain-on-failure',
+    },
+    expect: {
+        toMatchSnapshot: { threshold },
     },
     projects: [
         {
-            ...devices[ 'Desktop Firefox' ],
             name: 'Firefox',
-            use: { browserName: 'firefox' }
+            ...devices[ 'Desktop Firefox' ],
         },
         {
+            name: 'Chromium',
             ...devices[ 'Desktop Chrome' ],
-            name: 'Chrome',
-            use: { browserName: 'chromium' }
         },
         {
-            ...devices[ 'iPhone 12' ],
+            name: 'Edge',
+            ...devices[ 'Desktop Edge' ],
+        },
+        {
+            name: 'Safari',
+            ...devices[ 'Desktop Safari' ],
+        },
+        {
             name: 'iPhone',
-            use: { browserName: 'webkit' }
+            ...devices[ 'iPhone 13' ],
+        },
+        {
+            name: 'Android',
+            ...devices[ 'Pixel 5' ],
         }
     ]
 };
